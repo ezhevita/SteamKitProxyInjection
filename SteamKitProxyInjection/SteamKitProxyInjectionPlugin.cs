6af3ec3857d13216e760ba29430ab75ad1a44ff0
Vitaliya Chumakova <vichuma9@gmail.com>
2021-08-17 00:00:47 +0300

    Rework project system

--- SteamKitProxyInjection/SteamKitProxyInjectionPlugin.cs
+++ SteamKitProxyInjection/SteamKitProxyInjectionPlugin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Composition;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Net;
 using System.Net.WebSockets;
 using System.Reflection;
@@ -14,9 +15,24 @@ using Newtonsoft.Json.Linq;
 namespace SteamKitProxyInjection {
 	[Export(typeof(IPlugin))]
 	[UsedImplicitly]
-	public class SteamKitProxyInjection : IASF {
+	public class SteamKitProxyInjectionPlugin : IASF {
 		public void OnLoaded() {
-			ASF.ArchiLogger.LogGenericInfo("Loaded " + Name + " by Vital7 | Support & source code: https://github.com/Vital7/SteamKitProxyInjection");
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			string repository = assembly
+				.GetCustomAttributes<AssemblyMetadataAttribute>()
+				.First(x => x.Key == "RepositoryUrl")
+				.Value ?? throw new InvalidOperationException(nameof(AssemblyMetadataAttribute));
+
+			const string git = ".git";
+			int index = repository.IndexOf(git, StringComparison.Ordinal);
+			if (index >= 0) {
+				repository = repository[..(index + 1)];
+			}
+
+			string company = assembly
+				.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company ?? throw new InvalidOperationException(nameof(AssemblyCompanyAttribute));
+
+			ASF.ArchiLogger.LogGenericInfo(Name + " by " + company + " | Support & source code: " + repository);
 		}
 
 		public string Name => nameof(SteamKitProxyInjection);
@@ -33,13 +49,18 @@ namespace SteamKitProxyInjection {
 			ASF.ArchiLogger.LogGenericTrace("Retrieving WebSocketContext constructor...");
 			ConstructorInfo constructor = AccessTools.Constructor(webSocketContextType, new[] {webSocketConnectionType, typeof(EndPoint)});
 			ASF.ArchiLogger.LogGenericTrace("Patching...");
-			harmony.Patch(constructor, postfix: new HarmonyMethod(AccessTools.Method(typeof(SteamKitProxyInjection), nameof(TargetMethod))));
+			harmony.Patch(constructor, postfix: new HarmonyMethod(AccessTools.Method(typeof(SteamKitProxyInjectionPlugin), nameof(TargetMethod))));
 			ASF.ArchiLogger.LogGenericInfo("Successfully injected!");
 		}
 
 		[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
 		[SuppressMessage("ReSharper", "InconsistentNaming")]
+#pragma warning disable CA1707
 		public static void TargetMethod(ClientWebSocket ___socket) {
+			if (___socket == null) {
+				throw new ArgumentNullException(nameof(___socket));
+			}
+
 			ASF.ArchiLogger.LogGenericTrace("Retrieving WebProxy config value...");
 			IWebProxy? webProxy = ASF.GlobalConfig?.WebProxy;
 			if (webProxy == null) {
@@ -49,5 +70,6 @@ namespace SteamKitProxyInjection {
 			ASF.ArchiLogger.LogGenericTrace("Setting proxy...");
 			___socket.Options.Proxy = webProxy;
 		}
+#pragma warning restore CA1707
 	}
 }
