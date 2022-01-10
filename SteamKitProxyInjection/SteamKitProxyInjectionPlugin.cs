7bd7ca612f0aaed43a214ee9c5f1c6080cbeff15
Vitaliya Chumakova <vichuma9@gmail.com>
2022-01-10 20:55:45 +0300

    Fix build

--- SteamKitProxyInjection/SteamKitProxyInjectionPlugin.cs
+++ SteamKitProxyInjection/SteamKitProxyInjectionPlugin.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Composition;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Net;
 using System.Net.WebSockets;
 using System.Reflection;
+using System.Threading.Tasks;
 using ArchiSteamFarm.Core;
 using ArchiSteamFarm.Plugins.Interfaces;
 using HarmonyLib;
@@ -16,29 +16,18 @@ namespace SteamKitProxyInjection {
 	[Export(typeof(IPlugin))]
 	[UsedImplicitly]
 	public class SteamKitProxyInjectionPlugin : IASF {
-		public void OnLoaded() {
-			Assembly assembly = Assembly.GetExecutingAssembly();
-			string repository = assembly
-				.GetCustomAttributes<AssemblyMetadataAttribute>()
-				.First(x => x.Key == "RepositoryUrl")
-				.Value ?? throw new InvalidOperationException(nameof(AssemblyMetadataAttribute));
+		public Task OnLoaded()
+		{
+			ASF.ArchiLogger.LogGenericInfo($"{Name} by Vital7 | Support & source code: https://github.com/Vital7/{Name}");
 
-			const string git = ".git";
-			int index = repository.IndexOf(git, StringComparison.Ordinal);
-			if (index >= 0) {
-				repository = repository[..(index + 1)];
-			}
-
-			string company = assembly
-				.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company ?? throw new InvalidOperationException(nameof(AssemblyCompanyAttribute));
-
-			ASF.ArchiLogger.LogGenericInfo(Name + " by " + company + " | Support & source code: " + repository);
+			return Task.CompletedTask;
 		}
 
 		public string Name => nameof(SteamKitProxyInjection);
 		public Version Version => Assembly.GetExecutingAssembly().GetName().Version ?? throw new InvalidOperationException();
 
-		public void OnASFInit(IReadOnlyDictionary<string, JToken>? additionalConfigProperties = null) {
+		public Task OnASFInit(IReadOnlyDictionary<string, JToken>? additionalConfigProperties = null)
+		{
 			ASF.ArchiLogger.LogGenericInfo("Injecting...");
 			Harmony harmony = new("tech.Vital7.SteamKitProxyInjection");
 
@@ -51,6 +40,8 @@ namespace SteamKitProxyInjection {
 			ASF.ArchiLogger.LogGenericTrace("Patching...");
 			harmony.Patch(constructor, postfix: new HarmonyMethod(AccessTools.Method(typeof(SteamKitProxyInjectionPlugin), nameof(TargetMethod))));
 			ASF.ArchiLogger.LogGenericInfo("Successfully injected!");
+
+			return Task.CompletedTask;
 		}
 
 		[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
